b9327f702dbde81ec01a3c8931d97c859df8c189
Mark <agranatmarkit@gmail.com>
2018-04-06 07:30:42 +0300

    Initial Commit

---
+++ SerafimText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SerafimText
+//
+//  Created by Марк on 06.04.2018.
+//  Copyright © 2018 Agranat Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SerafimText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SerafimText
+//
+//  Created by Марк on 06.04.2018.
+//  Copyright © 2018 Agranat Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
